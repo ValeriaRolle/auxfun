a37f5447a21e5a9a8f7b1977bc2cb53c58d461ab
ValeriaRolle <avleria77@gmail.com>
2022-08-09 13:51:29 +0200

    urlobs trailing quotation marks

--- R/urlobs.r
+++ R/urlobs.r
@@ -2,7 +2,7 @@ urlobs <- function(){
   require(stringr)
   
   url <- readClipboard()
-  url <- str_sub(url, 1,-1)
+  url <- str_sub(url, 2,-2)
   url <- gsub(" ", "%20", url)
   url <- gsub("\\", "/", url, fixed = TRUE)
   url <- paste("file:///", url, sep = "")
