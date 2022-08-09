cd4dcf878c079de3e6856945b4bc3b6e601b351b
ValeriaRolle <avleria77@gmail.com>
2022-08-09 10:17:21 +0200

    urlobs added to untitled
    since it was not as easy as adding the .R and help file in the right folders, let's try to add the function to a pre-existing r file.

--- R/Untitled.R
+++ R/Untitled.R
@@ -44,6 +44,18 @@ ggtoppt <- function(gg, export = FALSE, pptname = "exportedplots.pptx", width = 
 }
 
 
+urlobs <- function(){
+  require(stringr)
+  
+  url <- readClipboard()
+  url <- str_sub(url, 1,-1)
+  url <- gsub(" ", "%20", url)
+  url <- gsub("\\", "/", url, fixed = TRUE)
+  url <- paste("file:///", url, sep = "")
+  writeClipboard(url)
+  return(url)
+}
+
 
 fptable <- function(columna, var_name = "Variable", caption = "", na = TRUE, format = "markdown", digits = 3){
 
